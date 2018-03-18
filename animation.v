63185fd78c8f834911ff6b0b96e180ef8d921920
guycalledjeremy <guycalledjeremy@gmail.com>
2018-03-18 19:36:00 -0400

    Modified the top-level module animation and datapath.

--- animation.v
+++ animation.v
@@ -1,7 +1,6 @@
     module animation(
             CLOCK_50,
             reset,
-            load,
             go,
             // The ports below are for the VGA output.  Do not change.
       		VGA_CLK,   						//	VGA Clock
@@ -16,7 +15,6 @@
 
           input CLOCK_50;
           input reset;
-          input load;
           input go,
           // Declare your inputs and outputs here
           // Do not change the following outputs
@@ -29,14 +27,18 @@
           output	    [9:0]	VGA_G;	 		//	VGA Green[9:0]
           output	    [9:0]	VGA_B;   		//	VGA Blue[9:0]
 
+          wire [7:0] x;
+          wire [6:0] y;
+          wire [2:0] colour;
+
         frame	f0(
             .CLOCK_50(CLOCK_50),						//	On Board 50 MHz
             // Your inputs and outputs here
             .reset(reset),
-            .load(load),
             .go(go),
-            .x_v(8'b10100000),
-            .y_v(7'd0),
+            .x_v(x),
+            .y_v(y),
+            .c_v(colour),
             // The ports below are for the VGA output.  Do not change.
             .VGA_CLK(VGA_CLK),   						//	VGA Clock
             .VGA_HS(VGA_HS),							//	VGA H_SYNC
@@ -47,7 +49,7 @@
             .VGA_G(VGA_G),	 						//	VGA Green[9:0]
             .VGA_B(VGA_B);   						//	VGA Blue[9:0]
 
-        datapath d0();
+        datapath d0(x, y, colour);
 
         control c0();
 
@@ -55,7 +57,11 @@
 
     endmodule
 
-    module datapath();
+    module datapath(clk, resetn, plot, x_v, y_v, c_v);
+        input clk, resetn, plot;
+        output [7:0] x_v;
+        output [6:0] y_v;
+        output [2:0] c_v;
 
     endmodule
 
@@ -81,15 +87,17 @@
       reg [3:0] current_state, next_state;
 
       localparam  S_LOAD_FRAME   = 2'd0,
-                  S_CYCLE        = 2'd2,
-                  S_END_ANI      = 2'd3;
+                  S_CYCLE        = 2'd1,
+                  S_END_ANI      = 2'd2;
+                  S_END_STATE    = 2'd3;
 
       always@(*)
         begin: state_table
               case (current_state)
                   S_LOAD_FRAME: next_state = go ? S_LOAD_X_WAIT : S_LOAD_X;
                   S_CYCLE: next_state = ({&c_q} == 1'b0) ? S_LOAD_X : S_CYCLE_0;
-                  S_END_ANI: next_state = ;
+                  S_END_ANI: next_state = S_END_STATE;
+                  S_END_STATE: next_state = S_END_STATE;
               default:     next_state = S_LOAD_X;
           endcase
         end
