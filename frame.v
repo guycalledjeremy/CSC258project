86919e9651bdf8b0ca40f742286ace81397b53c0
guycalledjeremy <36554567+guycalledjeremy@users.noreply.github.com>
2018-03-20 20:59:28 -0400

    Modified frame.v

--- frame.v
+++ frame.v
@@ -1,11 +1,11 @@
 module frame	(
 		CLOCK_50,						   //	On Board 50 MHz
 		// My inputs and outputs here
-        resetn,
-        go,
-        erase,
-        x,
-        y,
+        //resetn,
+        //go,
+        //erase,
+        SW,
+        KEY,
 		// The ports below are for the VGA output.  Do not change.
 		VGA_CLK,   						   //	VGA Clock
 		VGA_HS,							   //	VGA H_SYNC
@@ -18,11 +18,11 @@ module frame	(
 	);
 
 	input		    CLOCK_50;				//	50 MHz
-    input           resetn;
-    input           go;
-    input           erase;
-	input	[7:0]	x;
-	input	[5:0] 	y;
+    // input           resetn;
+    // input           go;
+    // input           erase;
+	input	[7:0]	SW;
+	input	[3:0] 	KEY;
 	// Declare your inputs and outputs here
 	// Do not change the following outputs
 	output			VGA_CLK;   				//	VGA Clock
@@ -33,6 +33,23 @@ module frame	(
 	output	    [9:0]	VGA_R;   			//	VGA Red[9:0]
 	output	    [9:0]	VGA_G;	 			//	VGA Green[9:0]
 	output	    [9:0]	VGA_B;   			//	VGA Blue[9:0]
+
+	reg [7:0] x;
+	reg [5:0] y;
+
+	wire go;
+	wire erase;
+	wire resetn;
+	assign resetn = KEY[3];
+	assign go = !KEY[1];
+	assign erase = !KEY[2];
+
+	always @(posedge CLOCK_50) begin
+		if (!KEY[0]) begin
+			x <= SW[7:0];
+			y <= SW[5:0];
+		end
+	end
 
 	// Create the writeEn wires that are inputs to the controller.
     wire reset = resetn;
@@ -72,21 +89,21 @@ module frame	(
 	// for the VGA controller, in addition to any other functionality your design may require.
     wire next;
 	wire enable;
-    wire read_w, go_sw;
+    wire go_sw;
 
     // Instansiate datapath
 	// datapath d0(...);
-      datapath d0(CLOCK_50,reset,enable,erase,next, x, y, c_v, x_v, y_v, read_w, go_sw);
+      datapath d0(CLOCK_50,reset,enable,erase,next, x, y, c_v, x_v, y_v, go_sw);
 
     // Instansiate FSM control
     // control c0(...);
-	  control c0(read_w,go_sw,reset,CLOCK_50,enable,writeEn,next);
+	  control c0(go,go_sw,reset,CLOCK_50,erase,enable,writeEn,next);
 endmodule
 // end main module
 
 
 // Datapath module
-module datapath(clock, reset_n, enable1, erase, next_p, x, y, colour, x_v, y_v, read, go_s);
+module datapath(clock, reset_n, enable1, erase, next_p, x, y, colour, x_v, y_v, go_s);
     	input 			    reset_n, enable1, clock;
         input               erase;
         input               next_p;
@@ -94,75 +111,86 @@ module datapath(clock, reset_n, enable1, erase, next_p, x, y, colour, x_v, y_v, 
 		input   [7:0]   x;
 		input   [5:0]   y;
 		// The location of each pixel
-        output  [7:0]   x_v;
-        output  [5:0]   y_v;
-        output  [2:0]   colour;
-        output read = 1'b1;
-        output go_s = 1'b1;
+        output	reg  [7:0]   x_v;
+        output	reg  [5:0]   y_v;
+        output	reg  [2:0]  colour;
+        output reg go_s;
 
         // The datapath should be instansiating a RAM file, reading data about the
         // next pixel from it.
 
-        // if (erase) begin
-        //     c_v <- 3'b111;
-        // end
-        wire [7:0] xv;
-        wire [5:0] xv;
-        wire [2:0] cv;
-        getlocation g0(enable1, clock, reset_n, x, y, x_v, y_v, c_v);
+	reg e;
+        // wire [7:0] xv;
+        // wire [5:0] xv;
+        // wire [2:0] cv;
+        // getlocation g0(enable1, clock, reset_n, x, y, x_v, y_v, c_v);
 
-        assign x_v = xv;
-        assign y_v = yv;
-        assign colour = cv;
-
-endmodule
-// end Datapath module
-
-module getlocation(enable, clock, resetn, x, y, x_v, y_v, c_v);
-    input enable, clock, resetn;
-    input [7:0] x;
-    input [5:0] y;
 	reg [2:0] x_i;
 	reg [2:0] y_i;
-    output reg [7:0]  x_v;
-    output reg [5:0] y_v;
-    output reg [2:0] c_v;
 
-    assign c_v = 3'b111;
-
-    always (posedge clock) begin
-		if (enable) begin
-	        if (!resetn) begin
+	    always @(posedge clock) begin
+	if (enable1) begin
+	        if (!reset_n) begin
+						x_i <= 3'd0;
+					y_i <= 3'd0;
 	            x_v <= x;
 	            y_v <= y;
+		go_s <= 1'b0;
+		colour <= 3'd0;
+		e <= 1'b0;
 	        end
 	        else begin
-	            if (y_i == 6'd4) begin
+			if (erase) begin
+				e <= 1'b1;		
+			end
+		    if (e) begin
+			colour <= 3'b111;
+		    end
+	            if (y_i == 3'd5) begin
 					y_i <= 3'd0;
 	                y_v <= y;
+					go_s <= 1'b1;	
 	            end
 	            else begin
-	                if (x_i == 8'd4) begin
+	                if (x_i == 3'd4) begin
 						x_i <= 3'd0;
 	                    x_v <= x;
 						y_i <= y_i + 1;
-	                    y_v <= y_v + y_i;
+	                    y_v <= y + y_i + 1;
 	                end
 	                else begin
 						x_i <= x_i + 1;
-	                    x_v <= x + x_i;
+	                    x_v <= x + x_i + 1;
 	                end
 	            end
 	        end
 		end
     end
+        // assign x_v = xv;
+        // assign y_v = yv;
+        // assign colour = cv;
 
 endmodule
+// end Datapath module
+
+//module getlocation(enable, clock, resetn, x, y, x_v, y_v, c_v);
+ //   input enable, clock, resetn;
+ //   input [7:0] x;
+ //   input [5:0] y;
+    //output reg [7:0]  x_v;
+ //   output reg [5:0] y_v;
+//    output [2:0] c_v;
+
+//    assign c_v = 3'b111;
+
+
+
+//endmodule
 
 
 // Control module
-module control(read, go_s,reset_n,clock,enable,plot,next_p);
-		input read,go_s,reset_n,clock;
+module control(read, go_s,reset_n,clock,erase,enable,plot,next_p);
+		input read,go_s,reset_n,clock,erase;
 		output reg enable,plot,next_p;
 
 		reg [3:0] current_state, next_state;
@@ -174,7 +202,7 @@ module control(read, go_s,reset_n,clock,enable,plot,next_p);
 		always@(*)
         begin: state_table
             case (current_state)
-                S_READ_FILE: next_state = (read == 1'b1) ? S_CYCLE : S_READ_FILE;
+                S_READ_FILE: next_state = (read || erase == 1'b1) ? S_CYCLE : S_READ_FILE;
                 S_CYCLE: next_state = (go_s == 1'b1) ? S_END_DRAW : S_CYCLE;
                 S_END_DRAW: next_state =  S_READ_FILE;
             default:     next_state = S_READ_FILE;
@@ -184,7 +212,7 @@ module control(read, go_s,reset_n,clock,enable,plot,next_p);
 		always@(*)
         begin: enable_signals
         // By default make all our signals 0
-          next = 1'b0;
+          next_p = 1'b0;
 		  enable = 1'b0;
 		  plot = 1'b0;
 
@@ -195,7 +223,7 @@ module control(read, go_s,reset_n,clock,enable,plot,next_p);
 				S_CYCLE:begin
                     enable = 1'b1;
 					plot = 1'b1;
-                    next = 1'b1;
+                    next_p = 1'b1;
 					end
                 S_END_DRAW:begin
                     // Maybe do nothing?
@@ -208,9 +236,10 @@ module control(read, go_s,reset_n,clock,enable,plot,next_p);
 		always@(posedge clock)
       begin: state_FFs
         if(!reset_n)
-            current_state <= S_LOAD_X;
+            current_state <= S_READ_FILE;
         else
             current_state <= next_state;
       end
 endmodule
 // end Control module
+
